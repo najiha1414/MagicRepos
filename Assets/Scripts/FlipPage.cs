0ae1ce2d99bdd9cfe232c6e3a425a13a7d99996c
Najiha1414 <najiha1414@gmail.com>
2021-11-23 12:43:01 +0800

    Book changes

---
+++ Assets/Scripts/FlipPage.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class FlipPage : MonoBehaviour
+{
+    private enum ButtonType
+    {
+        NextButton,
+        PrevButton
+    }
+
+    [SerializeField] Button nextBtn;
+    [SerializeField] Button prevBtn;
+
+    private Vector3 rotationVector;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private bool isClicked;
+    private DateTime startTime;
+    private DateTime endTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startRotation = transform.rotation;
+        startPosition = transform.position;
+
+        if (nextBtn != null)
+        nextBtn.onClick.AddListener(() => turnOnePageBtn_Click(ButtonType.NextButton));
+
+        if (nextBtn != null)
+        nextBtn.onClick.AddListener(() => turnOnePageBtn_Click(ButtonType.PrevButton));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClicked)
+        {
+            transform.Rotate(rotationVector * Time.deltaTime);
+            endTime = DateTime.Now;
+
+            if ((endTime - startTime).TotalSeconds >= 1)
+            {
+                isClicked = false;
+                transform.rotation = startRotation;
+                transform.position = startPosition;
+            }
+        }
+    }
+
+    private void turnOnePageBtn_Click(ButtonType type)
+    {
+        isClicked = true;
+        startTime = DateTime.Now;
+
+        if (type == ButtonType.NextButton)
+        {
+            rotationVector = new Vector3(0, 180, 0);
+        }
+
+        else if (type == ButtonType.PrevButton)
+        {
+            rotationVector = new Vector3(0, -180, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/OpenBook.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OpenBook : MonoBehaviour
+{
+    [SerializeField] Button openBtn;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip openBook;
+    private Vector3 rotationVector;
+    private bool isOpenClicked;
+    private DateTime startTime;
+    private DateTime endTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (openBtn != null)
+        openBtn.onClick.AddListener(() => openBtn_Click());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpenClicked)
+        {
+            transform.Rotate(rotationVector * Time.deltaTime);
+            endTime = DateTime.Now;
+
+            if ((endTime - startTime).TotalSeconds >= 1)
+            {
+                isOpenClicked = false;
+            }
+        }
+    }
+
+    private void openBtn_Click()
+    {
+        isOpenClicked = true;
+        startTime = DateTime.Now;
+        
+        rotationVector = new Vector3(0, 180, 0);
+
+        playSound();
+    }
+
+    private void playSound()
+    {
+        if ((audioSource != null) && (openBook != null))
+        {
+            audioSource.PlayOneShot(openBook);
+        }
+    }
+}
