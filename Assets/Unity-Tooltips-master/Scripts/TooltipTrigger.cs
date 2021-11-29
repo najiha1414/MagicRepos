0acac8768c521bcc572b7de2b11c89060e051857
Najiha1414 <najiha1414@gmail.com>
2021-11-29 17:31:36 +0800

    Tooltip

---
+++ Assets/Unity-Tooltips-master/Scripts/TooltipTrigger.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger))]
+public class TooltipTrigger : MonoBehaviour
+{
+    [SerializeField]
+    string mDisplayName;
+    
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        AddPointerEnterTrigger(trigger, EventTriggerType.PointerEnter);
+        AddEventTrigger(trigger, OnPointerExit, EventTriggerType.PointerExit);
+    }
+
+    private void AddPointerEnterTrigger(EventTrigger evTrig, EventTriggerType triggerType)
+    {
+        EventTrigger.TriggerEvent triggerEvent = new EventTrigger.TriggerEvent();
+        AddEventTrigger(evTrig, d => OnPointerEnter(d, evTrig.gameObject), EventTriggerType.PointerEnter);
+        EventTrigger.Entry entry = new EventTrigger.Entry() { callback = triggerEvent, eventID = triggerType };
+        evTrig.triggers.Add(entry);
+    }
+
+    private void AddEventTrigger(EventTrigger evTrig, UnityAction action, EventTriggerType triggerType)
+    {
+        EventTrigger.TriggerEvent triggerEvent = new EventTrigger.TriggerEvent();
+        triggerEvent.AddListener((eventData) => action());
+        EventTrigger.Entry entry = new EventTrigger.Entry() { callback = triggerEvent, eventID = triggerType };
+        evTrig.triggers.Add(entry);
+    }
+
+
+    private void AddEventTrigger(EventTrigger evTrig, UnityAction<BaseEventData> action, EventTriggerType triggerType)
+    {
+        EventTrigger.TriggerEvent triggerEvent = new EventTrigger.TriggerEvent();
+        triggerEvent.AddListener((eventData) => action(eventData));
+        EventTrigger.Entry entry = new EventTrigger.Entry() { callback = triggerEvent, eventID = triggerType };
+        evTrig.triggers.Add(entry);
+    }
+
+    private void OnPointerEnter(BaseEventData dataObject, GameObject hovered)
+    {
+        if (hovered != null)
+            Tooltip.Instance.SetTooltip(mDisplayName);
+
+    }
+
+    private void OnPointerExit()
+    {
+        Tooltip.Instance.HideTooltip();
+    }
+
+    public void SetTooltip(string aText)
+    {
+        mDisplayName = aText;
+    }
+}
